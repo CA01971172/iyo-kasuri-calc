253831503ea784b089b02dacb0ce92eb88d19269
CA01971172 <CA01971172@st.kawahara.ac.jp>
2026-01-15 11:56:24 +0900

    Implement AppStepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { AppStepper } from './components/AppStepper'
 import PhotoUploader from './PhotoUploader'
 
 export default function App() {
     return (
         <>
-            <PhotoUploader/>
+            <AppStepper/>
         </>
     )
 }
