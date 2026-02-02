7ab7cdd0ce8301c15403344626289973c5a2a2d2
CA01971172 <CA01971172@st.kawahara.ac.jp>
2026-02-02 12:37:02 +0900

    fix icons

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,15 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'fonts/*.ttf'],
+      // injectRegister: 'inline', // 登録を確実にするためのオプション（任意）
       manifest: {
-        name: '絣計測アプリ',
+        name: '伊予絣計測アプリ',
         short_name: '絣計測',
         description: '織物の絣を計測するための職人向けアプリ',
-        theme_color: '#1976d2',
+        theme_color: '#1B305D',
+        start_url: './',  // 現在のディレクトリから開始
+        scope: './',      // 現在のディレクトリを範囲にする
+        display: 'standalone',
         icons: [
           {
             src: 'pwa-192x192.png',
